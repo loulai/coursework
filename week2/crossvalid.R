fe56cda4d4d98ff1c104e21872cc33e3331c6554
Louise Lai <louiselai88@gmail.com>
2016-06-24 11:48:31 -0400

    trying to load trips

--- week2/crossvalid.R
+++ week2/crossvalid.R
@@ -4,13 +4,12 @@ library(lubridate)
 library(plyr)
 
 load('trips.RData')
-load('weather.RData')
 View(trips)
 View(weather)
 
 #======
 
-tday <- trips %>% group_by(ymd) %>% summarize(total_trips = n()) 
+tday <- trips %>% group_by(ymd) %>% summarize(total_trips) 
 tday <- join(tday, weather, "ymd")
 tday <- select(tday, ymd, tmin, total_trips)
 
